161b35a2cfee2e845b11eba0c494a6fdced94f9c
Eduardo Vargas <eduardo0407@gmail.com>
2012-09-19 15:55:45 -0300

    InstituicoesController

---
+++ application/controllers/InstituicoesController.php
@@ -0,0 +1,33 @@
+<?php
+
+class InstituicoesController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+    public function adicionarAction(){
+        $request = $this->getRequest();
+        $form = new Application_Form_Instituicoes();
+
+        if($this->getRequest()->isPost()){
+            if($form->isValid($request->getPost())){
+                echo "<pre>";
+                print_r($form->getValues());
+                echo "</pre>";
+            }
+        }
+
+        $this->view->form = $form;
+    }
+
+
+}
+
