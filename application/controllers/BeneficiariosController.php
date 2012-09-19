2e36f5d7f4843e446eff21bc83ad121a3dda0136
Eduardo Vargas <eduardo0407@gmail.com>
2012-09-19 16:17:41 -0300

    InstituicoesController

---
+++ application/controllers/BeneficiariosController.php
@@ -0,0 +1,33 @@
+<?php
+
+class BeneficiariosController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+    public function adicionarAction(){
+        $request = $this->getRequest();
+        $form = new Application_Form_Beneficiarios();
+
+        if($this->getRequest()->isPost()){
+            if($form->isValid($request->getPost())){
+                echo "<pre>";
+                print_r($form->getValues());
+                echo "</pre>";
+            }
+        }
+
+        $this->view->form = $form;
+    }
+
+
+}
+
