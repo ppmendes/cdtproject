6a7104364aecf045f16858382973514999e2d57b
Harley Vera Olivera <harve_17@hotmail.com>
2012-09-20 17:56:21 -0300

    Modificações de layout

--- application/models/DbTable/ViagemDetalhe.php
+++ application/models/DbTable/ViagemDetalhe.php
@@ -6,10 +6,10 @@ class Application_Model_DbTable_ViagemDetalhe extends Zend_Db_Table_Abstract
     protected $_name = 'viagem_detalhe'; // nome tabela no banco
 
     protected $_referenceMap    = array(
-        'Application_Model_DbTable_CategoriaFinanciador' => array(
-            'columns'           => 'categoria_financiador_id',
-            'refTableClass'     => 'Application_Model_DbTable_CategoriaFinanciador',
-            'refColumns'        => 'categoria_financiador_id'
+        'Application_Model_ViagemDetalhe' => array(
+            'columns'           => 'solicitacao_id',
+            'refTableClass'     => 'Application_Model_ViagemDetalhe',
+            'refColumns'        => 'solicitacao_id'
         ),
     );
 
