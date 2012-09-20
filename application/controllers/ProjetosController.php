a64e28f185dae25fe8cf4f29b2d2136527736956
Daniel <curydani@hotmail.com>
2012-09-20 17:34:23 -0300

    Models Daniel

--- application/controllers/ProjetosController.php
+++ application/controllers/ProjetosController.php
@@ -13,9 +13,21 @@ class ProjetosController extends Zend_Controller_Action
 	    $projetoModel = new Application_Model_Projeto();
         $projetoModel = $projetoModel->find(1);
         $prioridade = $projetoModel->findParentApplication_Model_DbTable_Prioridade();
+        $modoContratacao = $projetoModel->findParentApplication_Model_DbTable_ModoContratacao();
+        $projetoTipo = $projetoModel->findParentApplication_Model_DbTable_ProjetoTipo();
+        $instituicao = $projetoModel->findParentApplication_Model_DbTable_Instituicao();
+        $estado_projeto = $projetoModel->findParentApplication_Model_DbTable_EstadoProjeto();
+        $categoriaFinanciador = $projetoModel->findParentApplication_Model_DbTable_CategoriaFinanciador();
         echo "<pre>";
 
         echo "</pre>";
+        $this->view->prioridade = $prioridade;
+        $this->view->modoContratacao = $modoContratacao;
+        $this->view->projetoTipo = $projetoTipo;
+        $this->view->instituicao = $instituicao;
+        $this->view->estado_projeto = $estado_projeto;
+        $this->view->categoriaFinanciador = $categoriaFinanciador;
+
     }
 
     public function adicionarAction(){
