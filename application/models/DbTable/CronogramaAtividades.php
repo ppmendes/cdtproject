0406d295d60d937474cb55dc5c69da1de3c3c6f1
Daniel <curydani@hotmail.com>
2012-09-27 13:44:58 -0300

    Models Daniel - Atualização

--- application/models/DbTable/CronogramaAtividades.php
+++ application/models/DbTable/CronogramaAtividades.php
@@ -5,10 +5,10 @@ class Application_Model_DbTable_CronogramaAtividades extends Zend_Db_Table_Abstr
 
     protected $_name = 'cronograma_atividades';
     protected $_referenceMap    = array(
-        'Application_Model_DbTable_Solicitacao' => array(
-            'columns'           => 'solicitacao_id',
-            'refTableClass'     => 'Application_Model_DbTable_Solicitacao',
-            'refColumns'        => 'solicitacao_id'
+        'Application_Model_DbTable_Servicos' => array(
+            'columns'           => 'servicos_id',
+            'refTableClass'     => 'Application_Model_DbTable_Servicos',
+            'refColumns'        => 'servicos_id'
         ),
     );
 }
