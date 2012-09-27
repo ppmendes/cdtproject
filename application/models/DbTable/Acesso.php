36f6ee9e47cefe788006ddf69d0dc309e2da76d3
Eduardo Vargas <eduardo0407@gmail.com>
2012-09-27 13:46:36 -0300

    Atualização de Models com o BD - Eduardo

--- application/models/DbTable/Acesso.php
+++ application/models/DbTable/Acesso.php
@@ -5,7 +5,7 @@ class Application_Model_DbTable_Acesso extends Zend_Db_Table_Abstract
 
    	protected $_name = 'acesso';
 
-    protected $_referenceMap    = array(
+  /*  protected $_referenceMap    = array(
         'Application_Model_DbTable_Menu' => array(
             'columns'           => 'menu_id',
             'refTableClass'     => 'Application_Model_DbTable_Menu',
@@ -21,6 +21,6 @@ class Application_Model_DbTable_Acesso extends Zend_Db_Table_Abstract
             'refTableClass'     => 'Application_Model_DbTable_Acoes',
             'refColumns'        => 'acoes_id'
         ),
-    );
+    );       */
 }
 
--- application/models/DbTable/Beneficiario.php
+++ application/models/DbTable/Beneficiario.php
@@ -6,10 +6,35 @@ class Application_Model_DbTable_Beneficiario extends Zend_Db_Table_Abstract
    	protected $_name = 'beneficiario';
 
     protected $_referenceMap    = array(
+        'Application_Model_DbTable_Pais' => array(
+            'columns'           => 'pais_id',
+            'refTableClass'     => 'Application_Model_DbTable_Pais',
+            'refColumns'        => 'pais_id'
+        ),
+        'Application_Model_DbTable_Estados' => array(
+            'columns'           => 'estados_id',
+            'refTableClass'     => 'Application_Model_DbTable_Estados',
+            'refColumns'        => 'estados_id'
+        ),
+        'Application_Model_DbTable_Cidade' => array(
+            'columns'           => 'cidade_id',
+            'refTableClass'     => 'Application_Model_DbTable_Cidade',
+            'refColumns'        => 'cidade_id'
+        ),
         'Application_Model_DbTable_Banco' => array(
             'columns'           => 'banco_id',
             'refTableClass'     => 'Application_Model_DbTable_Banco',
             'refColumns'        => 'banco_id'
+        ),
+        'Application_Model_DbTable_Escolaridade' => array(
+            'columns'           => 'escolaridade_id',
+            'refTableClass'     => 'Application_Model_DbTable_Escolaridade',
+            'refColumns'        => 'escolaridade_id'
+        ),
+        'Application_Model_DbTable_AreaConhecimento' => array(
+            'columns'           => 'area_conhecimento_id',
+            'refTableClass'     => 'Application_Model_DbTable_AreaConhecimento',
+            'refColumns'        => 'area_conhecimento_id'
         ),
     );
 }
