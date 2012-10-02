93073e3bb5631eb7632173b91c5344e4cc88180c
Harley Vera Olivera <harve_17@hotmail.com>
2012-10-02 10:30:23 -0300

    Modificações de layout

--- application/controllers/InstituicoesController.php
+++ application/controllers/InstituicoesController.php
@@ -29,6 +29,29 @@ class InstituicoesController extends Zend_Controller_Action
         $this->view->form = $form;
     }
 
+    public function detalhesAction(){
+        $request = $this->getRequest();
+        $detalhes = new Application_Form_Instituicoes();
+        $model = new Application_Model_Instituicao;
+        $id = $this->_getParam('instituicao_id');
+
+        if($this->getRequest()->isPost()){
+            if($detalhes->isValid($request->getPost())){
+                echo "<pre>";
+                print_r($detalhes->getValues());
+                echo "</pre>";
+            }
+        }
+
+        $data = $model->find($id)->toArray();
+
+        if(is_array($data)){
+            $detalhes->setAction('/instituicao/detalhes/instituicao_id/' . $id);
+            $detalhes->populate(array("instituicao" => $data));
+        }
+
+        $this->view->detalhes = $detalhes;
+    }
 
 }
 
