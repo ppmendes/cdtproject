ccb1c310020a9eaf31a7f7970cc798c2df908fc0
Eduardo Vargas <eduardo0407@gmail.com>
2012-10-04 15:47:09 -0300

    Desembolso

--- application/controllers/DesembolsoController.php
+++ application/controllers/DesembolsoController.php
@@ -10,22 +10,66 @@ class DesembolsoController extends Zend_Controller_Action
 
     public function indexAction()
     {
+        $desembolsoModel = new Application_Model_Desembolso();
+        $this->view->desembolsos = $desembolsoModel->selectAll();
 
     }
 
     public function adicionarAction(){
         $request = $this->getRequest();
         $form = new Application_Form_Desembolso();
+        $model = new Application_Model_Desembolso;
+        $id = $this->_getParam('desembolso_id');
 
         if($this->getRequest()->isPost()){
             if($form->isValid($request->getPost())){
-                echo "<pre>";
-                print_r($form->getValues());
-                echo "</pre>";
+//                echo "<pre>";
+//                print_r($form->getValues());
+//                echo "</pre>";
+                $data = $form->getValues();
+
+                $model->insert($data);
+
+                $this->_redirect('/desembolso/');
             }
         }
 
         $this->view->form = $form;
+
+
+    }
+
+    public function detalhesAction(){
+        $request = $this->getRequest();
+        $detalhes = new Application_Form_Desembolso();
+        $model = new Application_Model_Desembolso;
+        $id = $this->_getParam('desembolso_id');
+        $this->view->id = $id;
+
+
+        $data = $model->find($id)->toArray();
+
+        if(is_array($data)){
+            $detalhes->setAction('/desembolso/detalhes/desembolso_id/' . $id);
+            $detalhes->populate(array("desembolso" => $data));
+        }
+
+        $this->view->detalhes = $detalhes;
+
+
+    }
+
+    public function excluirAction(){
+        //$request = $this->getRequest();
+        $excluir = new Application_Form_Desembolso();
+        $model = new Application_Model_Desembolso;
+        $id = $this->_getParam('desembolso_id');
+
+        $model->delete($id);
+        $this->_redirect('/desembolso/');
+
+        $this->view->excluir = $excluir;
+
     }
 
 
