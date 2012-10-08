f51322b6aa03a3343cbfec4d3bab85523e82bb82
Eduardo Vargas <eduardo0407@gmail.com>
2012-10-08 14:07:39 -0300

    Commit

--- application/models/Projeto.php
+++ application/models/Projeto.php
@@ -47,7 +47,8 @@ class Application_Model_Projeto
                 ->joinLeft(array('ep' => 'estado_projeto'), 'p.estado_projeto_id = ep.estado_projeto_id')
                 ->joinLeft(array('pr' => 'prioridade'), 'p.prioridade_id = pr.prioridade_id')
                 ->joinLeft(array('ct' => 'usuario'), 'p.coordenador_tecnico = ct.usuario_id',array('ct.usuario_id'=>'ct.usuario_id','ct.nome'=>'ct.nome','ct.sobrenome'=>'ct.sobrenome'))
-                ->joinLeft(array('ga' => 'instituicao'), 'p.gerencia = ga.instituicao_id',array('ga.instituicao_id'=>'ga.instituicao_id','ga.nome'=>'ga.nome'));
+                ->joinLeft(array('ga' => 'instituicao_gerencia'), 'p.instituicao_gerencia_id = ga.instituicao_gerencia_id',array(
+                'ga.instituicao_gerencia_id'=>'ga.instituicao_gerencia_id','ga.nome_instituicao_gerencia'=>'ga.nome_instituicao_gerencia'));
             $stmt = $select->query();
 
             $result = $stmt->fetchAll();
