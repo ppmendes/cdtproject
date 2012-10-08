7a5dde5d06c1ef730a8dbae99e0f7004c07ca246
Daniel <curydani@hotmail.com>
2012-10-08 16:46:30 -0300

    Teste

--- application/models/Tarefa.php
+++ application/models/Tarefa.php
@@ -11,27 +11,61 @@ class Application_Model_Tarefa
 
     public function insert($data)
     {
-        $table = new Application_Model_DbTable_Tarefa();
+        $table = new Application_Model_DbTable_Tarefa;
         $table->insert($data['tarefa']);
     }
 
     public function delete($id)
     {
+        $db = Zend_Db_Table::getDefaultAdapter();
+        $table = "tarefa";
+        $deletado = true;
+        $where = $db->quoteInto('tarefa_id = ?', $id);
+        $data = array('deletado' => $deletado);
+
+        $db->update($table, $data, $where);
 
     }
 
-    public function update($tarefa)
+    public function update($data, $id)
     {
 
+        $table = new Application_Model_DbTable_Tarefa;
+        $where = $table->getAdapter()->quoteInto('tarefa_id = ?',$id);
+
+        $table->update($data['tarefa'],$where);
+    }
+
+    public function selectAll()
+    {
+        try{
+            $db = Zend_Db_Table::getDefaultAdapter();
+
+            $select = $db->select()
+                ->from(array('t' => 'tarefa'))
+                ->where('t.deletado = ?', false)
+                ->joinLeft(array('us' => 'usuario'), 't.criador = us.usuario_id',array('us.usuario_id'=>'us.usuario_id','us.nome'=>'us.nome'))
+                ->joinLeft(array('pr' => 'projeto'), 't.projeto_id = pr.projeto_id',array('pr.projeto_id'=>'pr.projeto_id','pr.nome'=>'pr.nome'));
+//                ->joinLeft(array('ct' => 'usuario'), 'p.coordenador_tecnico = ct.usuario_id',array('ct.usuario_id'=>'ct.usuario_id','ct.nome'=>'ct.nome','ct.sobrenome'=>'ct.sobrenome'))
+//                ->joinLeft(array('ga' => 'instituicao_gerencia'), 'p.instituicao_gerencia_id = ga.instituicao_gerencia_id',array(
+//                'ga.instituicao_gerencia_id'=>'ga.instituicao_gerencia_id','ga.nome_instituicao_gerencia'=>'ga.nome_instituicao_gerencia'));
+            $stmt = $select->query();
+
+            $result = $stmt->fetchAll();
+
+            return $result;
+        }catch(Exception $e){
+            echo $e->getMessage();
+        }
     }
 
     public static function getOptions(){
         try{
             $options = array();
-            $table = new Application_Model_DbTable_Tarefa();
+            $table = new Application_Model_DbTable_Projeto();
             $resultado = $table->fetchAll();
             foreach($resultado as $item){
-                $options[$item['tarefa_id']] = $item['nome'];
+                $options[$item['projeto_id']] = $item['nome'];
             }
             return $options;
         } catch(Exception $e){
@@ -40,3 +74,4 @@ class Application_Model_Tarefa
 
     }
 }
+
