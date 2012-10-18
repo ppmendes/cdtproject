5724c9aac0eecb216dcfdb29487fc80c245f2af1
Eduardo Vargas <eduardo0407@gmail.com>
2012-10-18 15:41:44 -0300

    Termo Aditivo

--- application/models/DbTable/TermoAditivo.php
+++ application/models/DbTable/TermoAditivo.php
@@ -3,11 +3,35 @@
 class Application_Model_DbTable_TermoAditivo extends Zend_Db_Table_Abstract
 {
 
-   	protected $_name = 'termoaditivo';
+   	protected $_name = 'termo_aditivo';
 
+    protected $_referenceMap    = array(
+        'Application_Model_DbTable_Usuario' => array(
+            'columns'           => 'usuario_id',
+            'refTableClass'     => 'Application_Model_DbTable_Usuario',
+            'refColumns'        => 'usuario_id'
+        ),
+        'Application_Model_DbTable_TipoTermoAditivo' => array(
+            'columns'           => 'tipo_termo_aditivo_id',
+            'refTableClass'     => 'Application_Model_DbTable_TipoTermoAditivo',
+            'refColumns'        => 'tipo_termo_aditivo_id'
+        ),
+        'Application_Model_DbTable_Projeto' => array(
+            'columns'           => 'projeto_id',
+            'refTableClass'     => 'Application_Model_DbTable_Projeto',
+            'refColumns'        => 'projeto_id'
+        ),
+        'Orcamento_ID_Fonte' => array(
+            'columns'           => 'orcamento_id_fonte',
+            'refTableClass'     => 'Application_Model_DbTable_Orcamento',
+            'refColumns'        => 'orcamento_id'
+        ),
+        'Orcamento_ID_Destino' => array(
+            'columns'           => 'orcamento_id_destino',
+            'refTableClass'     => 'Application_Model_DbTable_Orcamento',
+            'refColumns'        => 'orcamento_id'
+        ),
 
-//    protected $_referenceMap    = array(
-//
-//    );
+    );
 }
 
