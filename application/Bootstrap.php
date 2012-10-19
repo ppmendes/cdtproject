8082d4ce555538a924b58126fafe0c17368e9e26
pedroengredes@gmail.com <pedroengredes@gmail.com>
2012-10-19 14:20:22 -0300

    Colocando o JQuery JQueryUI sempre no head

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -34,7 +34,15 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
     protected function _initJQuery()
     {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
         $this->view->addHelperPath('ZendX/JQuery/View/Helper/', 'ZendX_JQuery_View_Helper');
+        $view->jQuery()->enable()//enable jquery ; ->setCdnSsl(true) if need to load from ssl location
+                       ->setVersion('1.8')//jQuery version, automatically 1.5 = 1.5.latest
+                       ->setUiVersion('1.8')//jQuery UI version, automatically 1.8 = 1.8.latest
+                       //->addStylesheet('https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/ui-lightness/jquery-ui.css')//add the css
+                       ->uiEnable();//enable ui
+
     }
 
 }
