31799b603ac0f1ea662a29e1acd1378278944693
maria laura chavez cabrera <larakova@gmail.com>
2012-11-08 14:19:51 -0200

    atualizacao de usuarios

--- application/controllers/UsuariosController.php
+++ application/controllers/UsuariosController.php
@@ -13,6 +13,7 @@ class UsuariosController extends Zend_Controller_Action
     {
         $usuarioModel = new Application_Model_Usuario();
         $this->view->usuario = $usuarioModel->selectAll();
+        $this->view->usuariocontatos = $usuarioModel->selectAllcontatos();
     }
 
     public function adicionarAction(){
@@ -60,6 +61,51 @@ class UsuariosController extends Zend_Controller_Action
 
         $this->view->form = $form;
     }
+    public function adicionarcontatosAction(){
+        $request = $this->getRequest();
+        $form = new Application_Form_Usuarios_Contatos();
+        $model = new Application_Model_Usuario;
+        $id = $this->_getParam('usuario_id');
+        $this->view->pais = 76;
+        $form->getElement("estados_id")->setRegisterInArrayValidator(FALSE);
+        $form->getElement("cidade_id")->setRegisterInArrayValidator(FALSE);
+        $upload = new Zend_File_Transfer_Adapter_Http();
+
+        if($this->getRequest()->isPost()){
+            if($form->isValid($request->getPost())){
+
+                $data = $form->getValues();
+                //desabilita o atributo verifypassword
+                unset($data['usuario']['verifypassword']);
+                if($id){
+
+                    //adicionar novo arquivo e tirar o antigo na lixeira
+                    $newdata=$model->verificarMudancasArquivos($data,$id);
+                    // finalmente atualizamos o banco de dados
+                    $model->update($newdata, $id);
+                }else{
+                    if($data['usuario']['icone']!="")
+                    {
+                        $nome_imagem=$model->getLastInsertedId();
+                        $data=$model->editarImagem($nome_imagem,$data);
+                    }
+                    $model->insert($data);
+
+                }
+                $this->_redirect('/usuarios/');
+            }
+        }elseif ($id){
+            $data = $model->find($id)->toArray();
+            $this->view->pais = $data['pais_id'];
+
+            if(is_array($data)){
+                $form->setAction('/usuarios/detalhescontatos/usuario_id/' . $id);
+                $form->populate(array("usuario" => $data));
+            }
+        }
+
+        $this->view->form = $form;
+    }
 
     public function detalhesAction(){
         $request = $this->getRequest();
@@ -79,6 +125,23 @@ class UsuariosController extends Zend_Controller_Action
         $this->view->detalhes = $detalhes;
     }
 
+    public function detalhescontatosAction(){
+        $request = $this->getRequest();
+        $detalhes = new Application_Form_Usuarios_Contatos();
+        $model = new Application_Model_Usuario;
+        $id = $this->_getParam('usuario_id');
+        $this->view->id = $id;
+
+
+        $data = $model->find($id)->toArray();
+
+        if(is_array($data)){
+            $detalhes->setAction('/usuarios/detalhescontatos/usuario_id/' . $id);
+            $detalhes->populate(array("usuario" => $data));
+        }
+
+        $this->view->detalhes = $detalhes;
+    }
     /*public function atualizar(){
         //$request = $this->getRequest();
         $atualizar = new Application_Form_Instituicoes();
@@ -95,6 +158,18 @@ class UsuariosController extends Zend_Controller_Action
     public function excluirAction(){
         //$request = $this->getRequest();
         $excluir = new Application_Form_Usuarios();
+        $model = new Application_Model_Usuario;
+        $id = $this->_getParam('usuario_id');
+
+        $model->delete($id);
+
+        $this->_redirect('/usuarios/');
+
+        $this->view->excluir = $excluir;
+    }
+    public function excluircontatosAction(){
+        //$request = $this->getRequest();
+        $excluir = new Application_Form_Usuarios_Contatos();
         $model = new Application_Model_Usuario;
         $id = $this->_getParam('usuario_id');
 
--- application/forms/usuarios/Contatos.php
+++ application/forms/usuarios/Contatos.php
@@ -1,6 +1,6 @@
 <?php
 
-class Application_Form_Usuarios extends Zend_Form
+class Application_Form_Usuarios_Contatos extends Zend_Form
 {
     public function init()
     {
