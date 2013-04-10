1ccafce05880f924f3f1c500e85837b735b4d0a7
harley <harve_17@hotmail.com>
2013-04-10 09:35:19 -0300

    permissoes

--- application/controllers/UsuariosController.php
+++ application/controllers/UsuariosController.php
@@ -289,7 +289,6 @@ class UsuariosController extends Zend_Controller_Action
             echo 'entro!!';
             $datos=$this->getRequest()->getParams();
             $datostreeview=$datos['datostree'];
-            print_r($datostreeview);
             $modelpermissões->delete($id_usuario);
 
 
@@ -307,6 +306,7 @@ class UsuariosController extends Zend_Controller_Action
             //verificamos se o usuario é novinho ou já é um usuario registrado, por o numeros de projeto cadastrado
             $db = Zend_Db_Table::getDefaultAdapter();
             $result = $db->fetchAll("SELECT controller, action, valor FROM `permissao-usuario` WHERE usuario_id=$id_usuario");
+            $nomeprojeto= $db->fetchAll("SELECT distinct PU.valor, P.nome FROM `permissao-usuario` as PU inner join projeto as P on PU.valor=P.projeto_id");
 
             $permissoes=null;
             //concatenamos os valores da consulta
@@ -316,6 +316,7 @@ class UsuariosController extends Zend_Controller_Action
             }
 
             $this->view->permissoes=$permissoes;
+            $this->view->nomeprojetos=$nomeprojeto;
             echo 'nao entro :(';
         }
     }
