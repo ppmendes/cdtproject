5531bb706fbdf534ce5e5ddb0a8fa61b1be59c97
Daniel <curydani@hotmail.com>
2013-04-17 09:52:16 -0300

    Merge branch 'master' of C:\wamp\www\cdtproject with conflicts.

--- library/Plugin/Auth.php
+++ library/Plugin/Auth.php
@@ -26,6 +26,7 @@ class Plugin_Auth extends Zend_Controller_Plugin_Abstract{
             //array com as permissões do usuário
             $permissoes = $data->permissoes;
 
+            $permissoes['*']['*']['*'] = true;
 
             //controlador, ação e parâmetros da url acessada
             $controller = $request->getControllerName();
@@ -42,16 +43,12 @@ class Plugin_Auth extends Zend_Controller_Plugin_Abstract{
             if($controller == 'index'){
                 $permitirAcesso = true;
             }
-
             if(isset($permissoes[$controller][$acao][$projeto_id])
                 && $permissoes[$controller][$acao][$projeto_id] === true){
                 $permitirAcesso = true;
             }else if(isset($permissoes[$controller][$acao]['*'])
                 && $permissoes[$controller][$acao]['*'] === true){
                 $permitirAcesso = true;
-            }else if(isset($permissoes[$controller][$acao]['#'])
-                && $permissoes[$controller][$acao]['#']==true){
-                $permitirAcesso=true;
             }else if(isset($permissoes[$controller]['*']['*'])
                 && $permissoes[$controller]['*']['*'] === true){
                 $permitirAcesso = true;
