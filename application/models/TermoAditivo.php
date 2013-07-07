3ec3528f0dfc54df590ca5758ae83f46cd063187
Eduardo Vargas <eduardo0407@gmail.com>
2013-07-07 20:39:03 -0300

    termo aditivo

--- application/models/TermoAditivo.php
+++ application/models/TermoAditivo.php
@@ -11,46 +11,103 @@ class Application_Model_TermoAditivo
 
     public function insertAlterar($data)
     {
+        $table_termo_aditivo = new Application_Model_DbTable_TermoAditivo;
+        $table_orcamentos = new Application_Model_DbTable_Orcamento;
+
+        $orcamento = $data['termo_aditivo']['orcamento_destino'];
+        $orcamentodata = $table_orcamentos->find($orcamento)->current();
+
+        $rubrica_id = $orcamentodata['rubrica_id'];
+
         $str = $data['termo_aditivo']['valor_termino_aditivo'];
         $var = str_replace(".", "", $str);
         $var2 = str_replace(",", ".",$var);
         $data['termo_aditivo']['valor_termino_aditivo'] = $var2;
         $data['termo_aditivo']['data_termo_aditivo'] = date('Y-m-d H:i:s', time());
 
-
-        $table_termo_aditivo = new Application_Model_DbTable_TermoAditivo;
-        $table_orcamentos = new Application_Model_DbTable_Orcamento;
+        //Pegar o ID da rubrica para checar se é do tipo .36 e deve alterar também uma .47 correspondente
+        $arrayCodigoRubrica = $this->getCodigoRubrica($rubrica_id);
+        $codigoRubrica = $arrayCodigoRubrica[0]['r.codigo_rubrica'];
+        $rubrica = explode(".", $codigoRubrica);
+        $valor_imposto = 0.2 * $data['termo_aditivo']['valor_termino_aditivo'];
 
         $table_termo_aditivo->insert($data['termo_aditivo']);
         $table_orcamentos->update(array('valor_orcamento' => $data['termo_aditivo']['valor_termino_aditivo']), 'orcamento_id = ' . $data['termo_aditivo']['orcamento_destino']);
+
+        if ($rubrica[1] == '36')
+        {
+            if ($rubrica[2] != '02' && $rubrica[2] != '03' && $rubrica[2] != '07' && $rubrica[2] != '46' && $rubrica[2] != '80')
+            {
+                $table_orcamentos->update(array('valor_orcamento'=>$valor_imposto), 'orcamento_rel = ' . $data['termo_aditivo']['orcamento_destino']);
+            }
+
+        }
+
     }
 
     public function insertRemanejar($data)
     {
+        $table_termo_aditivo = new Application_Model_DbTable_TermoAditivo;
+        $table_orcamentos = new Application_Model_DbTable_Orcamento;
+
+        $orcamento_origem = $data['termo_aditivo']['orcamento_origem'];
+        $orcamento_destino = $data['termo_aditivo']['orcamento_destino'];
+        $orcamentodata_origem = $table_orcamentos->find($orcamento_origem)->current();
+        $orcamentodata_destino = $table_orcamentos->find($orcamento_destino)->current();
+
+        $rubrica_id_origem = $orcamentodata_origem['rubrica_id'];
+        $rubrica_id_destino = $orcamentodata_destino['rubrica_id'];
+
+
         $valor_orcamento_origem = $data['termo_aditivo']['valor_orcamento_origem'];
         $valor_orcamento_destino = $data['termo_aditivo']['valor_orcamento_destino'];
         unset($data['termo_aditivo']['valor_orcamento_origem']);
         unset($data['termo_aditivo']['valor_orcamento_destino']);
 
 
+        $arrayCodigoRubrica_origem = $this->getCodigoRubrica($rubrica_id_origem);
+        $arrayCodigoRubrica_destino = $this->getCodigoRubrica($rubrica_id_destino);
+        $codigoRubrica_origem = $arrayCodigoRubrica_origem[0]['r.codigo_rubrica'];
+        $codigoRubrica_destino = $arrayCodigoRubrica_destino[0]['r.codigo_rubrica'];
+        $rubrica_origem = explode(".", $codigoRubrica_origem);
+        $rubrica_destino = explode(".", $codigoRubrica_destino);
+
         $str = $data['termo_aditivo']['valor_termino_aditivo'];
         $var = str_replace(".", "", $str);
         $var2 = str_replace(",", ".",$var);
         $data['termo_aditivo']['valor_termino_aditivo'] = $var2;
         $data['termo_aditivo']['data_termo_aditivo'] = date('Y-m-d H:i:s', time());
 
-
-        $table_termo_aditivo = new Application_Model_DbTable_TermoAditivo;
-        $table_orcamentos = new Application_Model_DbTable_Orcamento;
+        $valor_orcamento_origem_atualizado = $valor_orcamento_origem - $data['termo_aditivo']['valor_termino_aditivo'];
+        $valor_orcamento_destino_atualizado = $valor_orcamento_destino + $data['termo_aditivo']['valor_termino_aditivo'];
+        $valor_imposto_origem = 0.2 * $valor_orcamento_origem_atualizado;
+        $valor_imposto_destino = 0.2 * $valor_orcamento_destino_atualizado;
 
         $table_termo_aditivo->insert($data['termo_aditivo']);
 
 
-        $table_orcamentos->update(array('valor_orcamento' => $valor_orcamento_origem - $data['termo_aditivo']['valor_termino_aditivo']),
+        $table_orcamentos->update(array('valor_orcamento' => $valor_orcamento_origem_atualizado),
                                         'orcamento_id = ' . $data['termo_aditivo']['orcamento_origem']);
 
-        $table_orcamentos->update(array('valor_orcamento' => $valor_orcamento_destino + $data['termo_aditivo']['valor_termino_aditivo']),
+        $table_orcamentos->update(array('valor_orcamento' => $valor_orcamento_destino_atualizado),
             'orcamento_id = ' . $data['termo_aditivo']['orcamento_destino']);
+
+
+        if ($rubrica_origem[1] == '36')
+        {
+            if ($rubrica_origem[2] != '02' && $rubrica_origem[2] != '03' && $rubrica_origem[2] != '07' && $rubrica_origem[2] != '46' && $rubrica_origem[2] != '80')
+            {
+                $table_orcamentos->update(array('valor_orcamento'=>$valor_imposto_origem), 'orcamento_rel = ' . $data['termo_aditivo']['orcamento_origem']);
+            }
+        }
+
+        if ($rubrica_destino[1] == '36')
+        {
+            if ($rubrica_destino[2] != '02' && $rubrica_destino[2] != '03' && $rubrica_destino[2] != '07' && $rubrica_destino[2] != '46' && $rubrica_destino[2] != '80')
+            {
+                $table_orcamentos->update(array('valor_orcamento'=>$valor_imposto_destino), 'orcamento_rel = ' . $data['termo_aditivo']['orcamento_destino']);
+            }
+        }
     }
 
     public function atualizaData($nova_data, $id)
@@ -107,6 +164,25 @@ class Application_Model_TermoAditivo
 
         return $var2;
 
+    }
+
+    public function getCodigoRubrica ($rid)
+    {
+        try{
+            $db = Zend_Db_Table::getDefaultAdapter();
+
+            $select = $db->select()
+                ->from(array('r' => 'rubrica'), array('r.codigo_rubrica' => 'r.codigo_rubrica'))
+                ->where('r.rubrica_id = ?', $rid);
+
+            $stmt = $select->query();
+            $resultado = $stmt->fetchAll();
+
+            return $resultado;
+
+        }catch (Exception $e){
+            echo $e->getMessage();
+        }
     }
 }
 
